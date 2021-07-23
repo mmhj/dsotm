5498c890d5d87acdaee980e4135d712e220a5ff4
mmhj <mhj@protonmail.com>
2021-07-23 14:48:31 +0200

    Improved information and overwrite
    Added more information for the user & -i flag to mv.

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/env sh
-#
+#!/bin/bash
+
+echo 'Installing Dark Side of the Moon theme for Sublime Text \n'
 
 subl_package_dir="${XDG_DATA_HOME:-$HOME/Library/Application Support/Sublime Text/Packages}"
 
-cd /tmp/dsotm && mv -t "$subl_package_dir" tmTheme/dsotm.tmTheme
+echo 'Moving the theme into Sublime Text packages \n'
+cd /tmp/dsotm && mv -t -i "$subl_package_dir" tmTheme/dsotm.tmTheme
+
+echo 'Installation complete! \n'
 
 echo 'To enable the color scheme:
 Navigate to Preferences -> Settings -> — User 
-and change the "color_scheme" setting to "color_scheme": "Packages/dsotm.tmTheme"'
+and change the "color_scheme" setting to "color_scheme": "Packages/dsotm.tmTheme"
+
+Enjoy the theme.'
