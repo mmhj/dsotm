5dd2053d6f7733c0d3df486b328d74e087b39094
mmhj <mhj@protonmail.com>
2021-07-23 14:07:26 +0200

    Fixed the installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 #
 
-subl_package_dir="${$HOME'/Library/Application Support/Sublime Text/Packages'}"
+subl_package_dir="${XDG_DATA_HOME:-$HOME/Library/Application\ Support/Sublime\ Text/Packages}"
 
-cd /tmp/dsotm
-mv -t "$subl_package_dir" tmTheme/dsotm.tmTheme
+cd /tmp/dsotm && mv -t "$subl_package_dir" tmTheme/dsotm.tmTheme
 
 echo 'To enable the color scheme:
 Navigate to Preferences -> Settings -> — User 
