75ab5d28ec05f96d725e1081ef9010fdc79ecb2f
mmhj <mhj@protonmail.com>
2021-08-06 17:55:11 +0200

    Slight formatting

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo '\n'
-echo 'Installing Dark Side of the Moon theme for Sublime Text \n'
+echo 'Installing Dark Side of the Moon colour scheme for Sublime Text \n'
 
 subl_package_dir="${XDG_DATA_HOME:-$HOME/Library/Application Support/Sublime Text/Packages}"
 
-cd /tmp/dsotm && mv -t "$subl_package_dir" tmTheme/dsotm.tmTheme
+cd /tmp/dsotm && cp -t "$subl_package_dir" tmTheme/dsotm.tmTheme
 
 echo 'Installation complete! \n'
 
-echo 'To enable the color scheme:
+echo 'To enable the colour scheme:
 Navigate to Preferences -> Settings -> — User 
 and change the "color_scheme" setting to "color_scheme": "Packages/dsotm.tmTheme"
 
-Enjoy the theme.'
+Enjoy.'
